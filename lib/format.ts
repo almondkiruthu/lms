9cae024819e7f81b58feded4b406c51421efe4dc
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-06 02:21:45 +0300

    Change prize format and gitignore

--- lib/format.ts
+++ lib/format.ts
@@ -1,6 +1,6 @@
 export const formatPrice = (price: number) => {
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("en-KE", {
     style: "currency",
-    currency: "USD"
+    currency: "KES"
   }).format(price)
 }
